62a439b4c689f1dc171d17c6c39141ef037e7c55
Khoa Nguyen Anh <k_nguyen@visionet.local>
2017-06-06 12:01:11 +0700

    merge

--- ext-script/crawler/index.js
+++ ext-script/crawler/index.js
@@ -2,10 +2,38 @@ var axios = require('axios'),
 	zing = require('./services/zing');
 
 
-	let temp = zing.getListArticle();
+zing.getListArticle();
+
+// console.log(pElement);
+// for(x on pElement){
+// 	console.log(pElement[x].text());
+// }
 
 
-	//CURL get list category
+/*var treeAdapter = parse5.treeAdapters.default;
+treeAdapter.getTagName = function(element){
+	 if(element.tagName.toLowerCase() == 'p'){
+	 	const stre = parse5.serialize(element);
+		console.log(stre);
+
+		// console.log(str);
+	 }
+	 return element.tagName;
+}
+
+const document = parse5.parse(str,  {
+    treeAdapter: treeAdapter
+});
+
+const html = parse5.serialize(document);
+
+// Serializes the <html> element content.
+const stre = parse5.serialize(document);
+console.log(stre);*/
+
+
+
+
 // let url = 'https://www.thongtincongnghe.com/views/ajax?js=1&page=1&view_name=v3_taxonomy_term&view_display_id=default&view_args=&view_path=article&view_base_path=&view_dom_id=3edc9f27f89e65f2e79026825164ec4a&pager_element=0';
 // axios.get(url)
 //     .then(function(response) {
--- ext-script/crawler/services/zing.js
+++ ext-script/crawler/services/zing.js
@@ -1,32 +1,71 @@
 //CURL get list category
 var axios = require('axios');
-var parse5 = require('parse5');
-getCategory = (url) => {
-	
-}
+var cheerio = require('cheerio');
+var firebase = require('firebase');
 
+var app = firebase.initializeApp({ 
+	apiKey: 'AIzaSyDu79SFQzwbQNBJjmJOKpgf_QPzPvhVV90',
+    authDomain: 'react-news-b1841.firebaseapp.com',
+    databaseURL: 'https://react-news-b1841.firebaseio.com',
+    storageBucket: 'react-news-b1841.appspot.com',
+    messagingSenderId: '25837712475'
+});
+
+
+let siteUrl = 'http://news.zing.vn';
+
+let arrCategory = ['/kinh-doanh-tai-chinh.html',
+					'/the-gioi.html',
+					'/thoi-su.html',
+					'/phap-luat.html',
+					'/the-thao.html',
+					];
+
+var userRef = firebase.database().ref('/users/0');
+  userRef.once('value').then(function(snapshot) {
+    var data = snapshot.val();
+   	console.log(data);
+   	app.delete(); // Release resources
+  }).catch(function(error) {
+    console.log('Failed to send notification to user:', error);
+  });
+
+
+// var defaultStorage = app.storage();
+// var defaultDatabase = app.database();
+// database = firebase.database();
+// // var starCountRef = database.ref('users/ewqdsadwqewqewq');
+// var leadsRef = database.ref('users');
+// leadsRef.on('value', function(snapshot) {
+//     console.log(snapshot);
+// });
 
 // //getListArticle
-// //url: https://www.thongtincongnghe.com/article
 getListArticle = () => {
-	axios.get('http://news.zing.vn/kinh-doanh-tai-chinh.html')
-		  .then(function (response) {
-				getArticle(response.data);
-		  })
-		  .catch(function (error) {
-		    console.log(error);
-		  });
+	arrCategory.forEach(function(value) {
+		let url = siteUrl + value;
+		// axios.get(url)
+		// 	  .then(function (response) {
+		// 			getBasicArticle(response.data);
+		// 	  })
+		// 	  .catch(function (error) {
+		// 	    	console.log(error);
+		// 	  });
+	})
 }
 
-getArticle = (html) => {
-	let documents = parse5.parse(html);
-	console.log(documents);
+//Get Basic Information Of Article Based On List
+getBasicArticle = (html) => {
+	let $ = cheerio.load(html);
+
+	$('section.cate_content article').each(function(index, element){
+		// console.log($(this).html());
+	});
 }
 
 
 // //export Module
 module.exports = {
-	getCategory: getCategory,
 	getListArticle: getListArticle,
 }
 
